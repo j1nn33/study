914311a4414c9b8d9fc7654fc7f9fc60fbe5c88c
Constantine <bkdjan@yandex.ru>
2017-11-18 13:33:19 +0000

    task 3_1 complete

--- python/tasks/3_1.py
+++ python/tasks/3_1.py
@@ -1,1 +1,9 @@
-print('qwe')
+
+#    Обработать строку NAT таким образом, чтобы в имени интерфейса вместо
+#    FastEthernet было GigabitEthernet.
+#    NAT = "ip nat inside source list ACL interface FastEthernet0/1 overload"
+
+NAT = "ip nat inside source list ACL interface FastEthernet0/1 overload"
+print (NAT)
+NEW_NAT = NAT.replace ('Fast', 'Gigabit')
+print (NEW_NAT)
