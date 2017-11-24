cdf001dfa7b6054cac9bacc081e7a4c0677ce555
Constantine <bkdjan@yandex.ru>
2017-11-24 19:52:27 +0000

    tasks 4_3 is complete

--- python/tasks/Базовые скрипты/4_3_a.py
+++ python/tasks/Базовые скрипты/4_3_a.py
@@ -1,18 +1,10 @@
 """
-Скрипт запрашиваeт у пользователя: 
-    информацию о режиме интерфейса (access/trunk),
-'Enter interface mode (access/trunk): '
-    номере интерфейса (тип и номер, вида Gi0/3)
-'Enter interface type and number: '
-    номер VLANа (для режима trunk будет вводиться список VLANов)
-'Enter vlan(s): '
-В зависимости от выбранного режима, на стандартный поток вывода
-возвращается соответствующая конфигурация access или trunk
-(шаблоны команд находятся в списках access_template и trunk_template).
-сначала идти строка interface и подставлен номер интерфейса, а
-затем соответствующий шаблон, в который подставлен номер VLANа (или список VLANов).
+Дополнить скрипт из задания 4.3 таким образом, чтобы, в зависимости от выбранного
+режима, задавались разные вопросы в запросе о номере VLANа или списка VLANов:
+для access: 'Enter VLAN number:'
+для trunk: 'Enter allowed VLANs:'
 """
-    # СПИСКИ и СЛОВАРИ
+# СПИСКИ и СЛОВАРИ
 vlan=[]                                                          # создаем пустой список
 vlan_t=0                                                         # инициализируем переменную vlan_t = 0 для access режима
 access_template = ['switchport mode access',                     # списки с режимами
@@ -25,14 +17,16 @@ trunk_template = ['switchport trunk encapsulation dot1q',
                   'switchport trunk allowed vlan {}']
 net_mode= {'access':access_template, 'trunk':trunk_template}    # словарь исппользуется для выбора режима
 vlan_list={'access':vlan_t, 'trunk':vlan}                       # словарь используется для выбора номера или списка vlan
+vlan_question={'access':'Enter VLAN number: ', 'trunk':'Enter allowed VLANs: '}                       # словарь используется для выбора номера или списка vlan
     # ВВЕДЕНИЕ ДАННЫХ
-int_mode=input('Enter interface mode (access/trunk):')
+#int_mode=input('Enter interface mode (access/trunk):')
 #int_mode = 'access'
-#int_mode = 'trunk'
-eth_type=input('Enter interface type and number: ')
-#eth_type = 'Fa0/7'
-vlan_t=int(input('Enter vlan(s): '))
-#vlan_t=3
+int_mode = 'trunk'
+#eth_type=input('Enter interface type and number: ')
+eth_type = 'Fa0/7'
+print(vlan_question[int_mode])                                  # выводится вопрос в зависимости от режима
+vlan_t=int(input())
+vlan_t=3
     # ОБРАБОТКА ДАННЫХ
 vlan.append(vlan_t)         # заполнение списка vlan
 vlan.append(4)
@@ -45,18 +39,3 @@ print('\n'.join(net_mode[int_mode]).format(vlan_list[int_mode]))
 # которая разделена символом \n , 
 # в строку подставляется (вместо {}  списки с режимами)
 # список (vlan_list) элемент (int_mode) , используя форматирование строк.
-
-"""
-interface Fa0/6
-switchport mode access
-switchport access vlan 3
-switchport nonegotiate
-spanning-tree portfast
-spanning-tree bpduguard 
-
-interface Fa0/7
-switchport trunk encapsulation dot1q
-switchport mode trunk
-switchport trunk allowed vlan 2,3,4,5
-"""
-
