aeb4fe1e30ba91b1dae4bc5181b2000e67cbbb77
Constantine <bkdjan@yandex.ru>
2019-05-22 09:39:18 +0000

    tune admin panel in djangoshop

--- python/django/djangoshop_v1/ecomapp/admin.py
+++ python/django/djangoshop_v1/ecomapp/admin.py
@@ -12,9 +12,16 @@ make_payed.short_description = "Пометить как оплаченные"
 class OrderAdmin(admin.ModelAdmin):
 	# фильтр статуса в админ панель в Order
 	list_filter = ['status']
+	list_display = ['id', 'items_in_order' ]
 	actions = [make_payed]
 
-
+    def items_in_order(self, obj):
+		items_in_order = '<br>'.join(['Товар - {0} | Кол-во - {1}'. format(item.product.title, item.qty) 
+		    for item in obj.items.items.all()])
+			
+		return items_in_order
+	
+	items_in_order.allow_tags = True
 
 
 
