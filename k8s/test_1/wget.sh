f03c2aa57e930066d644873c714e6ca9cb40be26
j1nn33 <bkdjan@gmail.com>
2024-01-02 12:02:37 +0300

    add file

---
+++ k8s/test_1/wget.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+cat <<EOF >/app/index.html
+<html>
+<head/>
+<body>
+<h1 style="color: #5e9ca0;"><span style="color: #000080;">TEST PAGE FOR WEB SERVER IN K8</span></h1>
+<p>&nbsp;</p>
+<!-- TEST PHRASE -->
+EOF
+
+INFO_ENV=$(export)
+INFO_MOUNTS=$(mount)
+INFO_RAM=$(free -m)
+INFO_RESOLV=$(cat /etc/resolv.conf)
+INFO_HOSTS=$(cat /etc/hosts)
+
+cat <<EOF >> /app/index.html
+<h3>Mountpoints</h3>
+<pre>$INFO_MOUNTS</pre>
+<h3>Environment</h3>
+<pre>$INFO_ENV</pre>
+<h3>Memory info</h3>
+<pre>$INFO_RAM</pre>
+<h3>DNS resolvers info</h3>
+<pre>$INFO_RESOLV</pre>
+<h3>Static hosts info</h3>
+<pre>$INFO_HOSTS</pre>
+</body>
+</html>
+EOF
